2ebfdf392a5ffecf0b7fcb4990c4dc0ec5391413
aacebo <44811138+aacebo@users.noreply.github.com>
2020-07-26 21:07:00 -0400

    add firebase hosting

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('npm-radar app is running!');
+    expect(page.getTitleText()).toEqual('dppkg app is running!');
   });
 
   afterEach(async () => {
