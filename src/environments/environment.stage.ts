175ae85fc81bb0a5be0d0c34cf8aedd18b825911
aacebo <44811138+aacebo@users.noreply.github.com>
2020-07-27 11:51:25 -0400

    fix staging

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -6,4 +6,14 @@ export const environment: IEnvironment = {
     pkg: 'http://localhost:5001/dppkg-451d0/us-central1/packages/package',
     search: 'http://localhost:5001/dppkg-451d0/us-central1/packages',
   },
+  firebase: {
+    apiKey: 'AIzaSyDiYiB10padjbaxn0K8bSatjQkLmzf17Dw',
+    authDomain: 'dppkg-451d0.firebaseapp.com',
+    databaseURL: 'https://dppkg-451d0.firebaseio.com',
+    projectId: 'dppkg-451d0',
+    storageBucket: 'dppkg-451d0.appspot.com',
+    messagingSenderId: '131434320995',
+    appId: '1:131434320995:web:68045b917a3336099b9456',
+    measurementId: 'G-KR1RZRCEL4',
+  },
 };
